6867137793fed3c64a7cdfdb4b7e888e42a599d5
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-23 14:52:18 +0700

    Add files via upload

---
+++ kode.62.cepar2.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+l = float(input("Masukan Nilai Panjang Gelombang (m) : "))
+f = float(input("Masukan Nilai Frekuensi (Hz) : "))
+
+# Mengkonversi
+v = l * f
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Cepat Rambat (m/s) = ',round(v,3))
+print('==========================================================')
---
+++ kode.62.cepar3.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+l = float(input("Masukan Nilai Panjang Gelombang (m) : "))
+t = float(input("Masukan Nilai Periode (s) : "))
+
+# Mengkonversi
+v = l / t
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Cepat Rambat (m/s) = ',round(v,3))
+print('==========================================================')
