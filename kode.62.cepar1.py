3ff17e63990e2677c938b23674aad37bb0c14513
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-23 14:37:02 +0700

    Add files via upload

---
+++ kode.62.cepar1.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+s = float(input("Masukan Nilai Jarak (m) : "))
+t = float(input("Masukan Nilai Waktu (s) : "))
+
+# Mengkonversi
+v = s / t
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Cepat Rambat (m/s) = ',round(v,3))
+print('==========================================================')
